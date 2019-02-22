d63d8cbf1c5af3b66d88a27280e373b3bfe6689f
Samarth <samarth.sanjay@ymail.com>
2019-02-22 08:59:17 -0500

    Used netlify to deploy

---
+++ build/precache-manifest.677eebdba0c96898fa1e421846c5588e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dbc48f507ff814f4e5a",
+    "url": "/VideoHub/static/js/runtime~main.8dbc48f5.js"
+  },
+  {
+    "revision": "6a09ac0ccbee9a03117f",
+    "url": "/VideoHub/static/js/main.6a09ac0c.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/VideoHub/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "6a09ac0ccbee9a03117f",
+    "url": "/VideoHub/static/css/main.1c5deeb0.chunk.css"
+  },
+  {
+    "revision": "a5a4994cad7e2cdaa41f00337fdf1d70",
+    "url": "/VideoHub/index.html"
+  }
+];
